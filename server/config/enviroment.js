4346db6343a4a4446c635c6fd1a9f943a9e6ce7d
Juan Carlos <jcortavee@gmail.com>
2020-07-15 23:43:49 -0600

    Enviroment variable

--- server/config/enviroment.js
+++ server/config/enviroment.js
@@ -6,6 +6,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'devs';
 
 // Database
 let database = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost:27017/cafe' :
-    'mongodb+srv://jcarlos21:1m42tLD79CByl2uX@cluster0.5mozg.mongodb.net/cafe'
+    process.env.MONGO_URL;
 
 process.env.URL_DATABASE = database;
